b77b18b0749357364ac92196d64c8229b9a7d9b4
coannanaufal-debug <coanna.naufal@gmail.com>
2026-06-28 18:14:59 +0700

    membuat implementasi static function dengan constructor dan destructor

---
+++ staticFunction.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string> //menyertakan library string
+using namespace std;
+
+class Mahasiswa {
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    //pembuatan static function
+    static void setNim(int pnim) {
+        nim = pnim;
+    }
+
+    static int getNim() {
+        return nim;
+    }
+
+    Mahasiswa(string pnama) :nama(pnama) {
+        setID();
+    }
+};
+
+int Mahasiswa::nim = 0;
+
+//pendefinisial prosedur - prosedur diluar class
+void Mahasiswa::setID() {
+    id = ++nim;
+}
+
+void Mahasiswa::printAll() {
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
