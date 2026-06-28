a2a3e7c4d826c94d032d0ee8dfdb43c67b6f895f
coannanaufal-debug <coanna.naufal@gmail.com>
2026-06-28 18:16:07 +0700

    add Mahasiswa class with static nim variable and methods

---
+++ staticMember.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa { //membuat class mahasiswa
+
+    public:
+    static int nim; //deklarasi variabel static yaitu variabel nim
+    //deklarasi variabel member
+    int id;
+    string nama;
+
+    //deklarasi prosedur setID() dan printAll()
+    void setID();
+    void printAll();
+
+    //pembuatan constructor Mahasiswa dengan parameter pnama
+    Mahasiswa(string pnama) :nama(pnama) {
+        setID();
+    }
+};
+
+//memberi value ke variabel nim dari class mahasiswa
+int Mahasiswa::nim = 10;
+
+//mengimplementasikan prosedur - prosedur di luar class
+void Mahasiswa::setID() {
+    id = ++nim;
+}
+
